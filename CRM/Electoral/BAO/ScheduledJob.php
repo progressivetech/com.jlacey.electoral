b57e25fb0652e9590658dac5d2a6199c5cf7a8f2
Jamie McClelland <jm@mayfirst.org>
2023-09-22 13:06:42 -0400

    adding boiler plate files from job entity

---
+++ CRM/Electoral/BAO/ScheduledJob.php
@@ -0,0 +1,8 @@
+<?php
+// phpcs:disable
+use CRM_Electoral_ExtensionUtil as E;
+// phpcs:enable
+
+class CRM_Electoral_BAO_ScheduledJob extends CRM_Electoral_DAO_ScheduledJob {
+
+}
---
+++ Civi/Api4/ScheduledJob.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * ScheduledJob entity.
+ *
+ * Provided by the Electoral API extension.
+ *
+ * @package Civi\Api4
+ */
+class ScheduledJob extends Generic\DAOEntity {
+
+}
